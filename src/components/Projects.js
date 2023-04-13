977e81fd4a5b4b27fb27037299a6f934e318a33f
Dave Estey <daveestey.working@gmail.com>
2023-04-13 03:23:24 -0300

    had to fix pictures

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,10 +9,9 @@ const Projects = (props) => {
               <div className="card-image ">
               <img className="full" src={project.img} alt="Picture of deployed site"/>
               <br/>
-              
-              <a href={project.deployed}>Deployment: {project.title}</a>
+              <a href={project.deployed}> {project.title}</a>
               <br />
-              <a href={project.github}>Github: <i className="fa fa-github"></i></a>
+             <a href={project.github}><i className="fa fa-github"></i></a>
             </div>
             </div>
             
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,37 +5,37 @@ const Portfolio = () => {
   const info = [
     {
       title: "Bootcamp Work Finder",
-      img: "https://raw.githubusercontent.com/DaveEstey/react-profile/main/public/bootcamp.png",
+      img: "https://raw.githubusercontent.com/DaveEstey/react-profile/main/src/images/bootcamp.png",
       github: "https://github.com/DaveEstey/bootcamp-work-finder/",
       deployed: "https://david-bootcamp-work-finder.herokuapp.com/",
     },
     {
       title: "Investment Solutions",
-      img: "https://raw.githubusercontent.com/DaveEstey/react-profile/main/public/investment.png",
+      img: "https://raw.githubusercontent.com/DaveEstey/react-profile/main/src/images/investment.png",
       github: "https://github.com/DaveEstey/Investment-Solutions",
       deployed: "https://daveestey.github.io/Investment-Solutions/",
     },
     {
       title: "Team Profile Generator",
-      img: "https://user-images.githubusercontent.com/114950818/212806907-9dea0b16-603d-4c44-ad12-25ebc3c32674.png",
+      img: "https://raw.githubusercontent.com/DaveEstey/react-profile/main/src/images/team_gen.png",
       github: "https://github.com/DaveEstey/Team-Profile-Generator",
       deployed: "https://github.com/DaveEstey/Team-Profile-Generator",
     },
     {
       title: "Body Buddy",
-      img: "https://raw.githubusercontent.com/DaveEstey/react-profile/main/public/body_buddy",
+      img: "https://raw.githubusercontent.com/DaveEstey/react-profile/main/src/images/body_buddy.png",
       github: "https://github.com/DaveEstey/body-buddy",
       deployed: "https://github.com/DaveEstey/body-buddy",
     },
     {
       title: "Text Editor",
-      img: "https://raw.githubusercontent.com/DaveEstey/react-profile/main/public/text_editor.png",
+      img: "https://raw.githubusercontent.com/DaveEstey/react-profile/main/src/images/text_edit.png",
       github: "https://github.com/DaveEstey/Text-Editor/",
       deployed: "https://text-editor-david.herokuapp.com/",
     },
     {
       title: "Social Networking",
-      img: "https://raw.githubusercontent.com/DaveEstey/react-profile/main/public/social_network.png",
+      img: "https://raw.githubusercontent.com/DaveEstey/react-profile/main/src/images/social_network.png",
       github: "https://github.com/DaveEstey/Social-Networking",
       deployed: "https://github.com/DaveEstey/Social-Networking",
     },
