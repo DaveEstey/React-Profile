d4bca7512d9d267df07d08d67d06304476025d90
DaveEstey <114950818+DaveEstey@users.noreply.github.com>
2023-04-25 18:01:34 -0300

    changed docx format

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -14,7 +14,7 @@ function About() {
       <br /> 
         Hello to whom is interested! My name is David Estey, born and raised in Fredericton, NB.
         <br />
-        I am just about to complete a Full Stack Developer Boot camp through UNB, and am ready to expand my knowledge in the feild of technology.
+        I just completed a Full Stack Developer Boot camp through UNB, and am ready to expand my knowledge in the feild of technology.
         <br />
         So far I have dipped my toes in game development, server development and website development. Hoping to get knowledge in ever expanding feilds such as: Quantum computing and AI integration.
         <br /> 
