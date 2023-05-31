5add6a915839cc37449de2a3fe954505962bb36d
DaveEstey <114950818+DaveEstey@users.noreply.github.com>
2023-05-31 01:46:01 -0300

    Updates

---
+++ precache-manifest.3f676bd0fe4572df3ae5570383287c86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a585f639c688caabf55a",
+    "url": "/react-profile/static/css/main.99ac9885.chunk.css"
+  },
+  {
+    "revision": "a585f639c688caabf55a",
+    "url": "/react-profile/static/js/main.a585f639.chunk.js"
+  },
+  {
+    "revision": "daf040836361c1388e7d",
+    "url": "/react-profile/static/js/1.daf04083.chunk.js"
+  },
+  {
+    "revision": "5b2c36fba0cda39a3764",
+    "url": "/react-profile/static/js/runtime~main.5b2c36fb.js"
+  },
+  {
+    "revision": "217c6a3181789a40ff7d3ae3ed4e17f6",
+    "url": "/react-profile/static/media/mugshot.217c6a31.jpg"
+  },
+  {
+    "revision": "b67bd69a9e1bf00d12d25b33954254b1",
+    "url": "/react-profile/index.html"
+  }
+];
